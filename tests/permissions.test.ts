bb080c34997cc5a2d6dfa66bc472765e4efd0723
dancer <josh@afterima.ge>
2026-02-10 10:36:42 -0600

    fixes

--- tests/permissions.test.ts
+++ tests/permissions.test.ts
@@ -11,6 +11,7 @@ import { cleanupTestDir, resetTestDir } from './helpers/mock-paths.js';
 import {
   addRule,
   clearRules,
+  invalidatePermissionsCache,
   isAllowed,
   listRules,
   removeRule,
@@ -19,6 +20,7 @@ import {
 describe('permissions', () => {
   beforeEach(() => {
     resetTestDir();
+    invalidatePermissionsCache();
   });
 
   afterEach(() => {
